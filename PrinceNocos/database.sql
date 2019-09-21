f946e3c3267e0d19c53816219b696bbded7df976
Prince <46000996+hangaroo27@users.noreply.github.com>
2019-09-21 15:24:57 +0800

    Add files via upload

---
+++ PrinceNocos/database.sql
@@ -0,0 +1,12 @@
+create database product;
+
+use product;
+
+CREATE TABLE `products` (
+  `id` int(255) NOT NULL auto_increment,
+  `pname` varchar(100) NOT NULL,
+  `pdescription` varchar(255) NOT NULL,
+  `pprice` int(255) NOT NULL,
+  `pquantity` int(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+);
